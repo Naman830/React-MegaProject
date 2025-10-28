c3e0c18639a3908228d6a3659c88900a2b0021ec
Naman Singla <namansingla830@gmail.com>
2025-10-28 21:38:36 +0530

    redux toolkit, create store and set up main and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  
-  return (
-    <>
-    <div className="min-h-screen bg-gray-100 p-4">
-      <h1 className="text-center text-4xl font-bold">
-        Hello Naman Working naman Vitereact
-      </h1>
+  const [loading, setLoading] = useState();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+
+      .catch((error) => {
+        console.log("Error from catch getCurrentUser", error);
+      })
+
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <dir className="w-full block">
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </dir>
     </div>
-    </>
-  );
+  ) : (null)
 }
 
 export default App;
