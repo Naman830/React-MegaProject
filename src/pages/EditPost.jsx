00d2e79975825b57ed1562fa36b6ec2d046de4cc
Naman Singla <namansingla830@gmail.com>
2025-10-30 20:57:43 +0530

    main.jsx properly fully completed

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,31 +1,30 @@
-import React, {useState, useEffect} from 'react'
-import { Container, PostCard, PostForm } from '../components'
+import React, { useState, useEffect } from "react";
+import { Container, PostCard, PostForm } from "../components";
 import appwriteService from "../appwrite/config";
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from "react-router-dom";
 
 const EditPost = () => {
-    const [post, setPosts] = useState();
-    const {slug} = useParams()
-    const navigate = useNavigate()
+  const [post, setPosts] = useState();
+  const { slug } = useParams();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (slug) {
-            appwriteService.getPost(slug).then((post) => {
-                if (post) {
-                    setPosts(post)
-                }
-            })
-        } else {
-            navigate('/')
+  useEffect(() => {
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => {
+        if (post) {
+          setPosts(post);
         }
-    }, [slug, navigate]);
+      });
+    } else {
+      navigate("/");
+    }
+  }, [slug, navigate]);
 
-    return post ? (
-        <div className="py-8">
-            <Container>
-                <PostForm post={post}/>
-            </Container>
-        </div>
-    ) : null
-
-}
+  return post ? (
+    <div className="py-8">
+      <Container>
+        <PostForm post={post} />
+      </Container>
+    </div>
+  ) : null;
+};
